90ac11ec8147cba68c182edc356dfa95d6040094
Débora Picado <dbpicado@gmail.com>
2021-03-31 02:11:13 +0000

    Carga del primer borrador

--- src/models.py
+++ src/models.py
@@ -8,23 +8,42 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Personajes(Base):
+    __tablename__ = 'personjes'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    personaje = Column(String(250), nullable=False)
+    genero = Column(String(250), nullable=False)
+    color_de_ojos = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Planetas(Base):
+    __tablename__ = 'planetas'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    diameter = Column(String(250)
+    population = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+  
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    mail= Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+
+class Favoritos(Base):
+    __tablename__ = 'planetas'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(250)
+    planeta_id = Column(String(250), nullable=False)
+    persona_id = Column(String(250), nullable=False)
+    
 
     def to_dict(self):
         return {}
